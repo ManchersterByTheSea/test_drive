a063ddba39718dc210b46ba7b8101ec622066856
Manchester.ding <1399589176@qq.com>
2021-12-21 16:33:12 +0800

    注册

---
+++ driver/driver.go
@@ -0,0 +1,23 @@
+package driver
+
+import (
+	"fmt"
+	"github.com/ManchersterByTheSea/test_common/register"
+)
+
+func init() {
+	register.Register("test", &Test{})
+}
+
+type Test struct {
+}
+
+func (t Test) Create() {
+	fmt.Println("Create test")
+
+}
+func (t Test) Update(a string, b int) error {
+
+	fmt.Println("Update test", a, b)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module test_drive
+
+go 1.15
+
+require github.com/ManchersterByTheSea/test_common v0.0.0-20211221071811-626d985ae6d3 // indirect
